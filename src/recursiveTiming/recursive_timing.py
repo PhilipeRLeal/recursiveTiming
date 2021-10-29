3d972e441bfbfc13c35ee91b1482589be5826c45
PhilipeRLeal <leal.philipe@gmail.com>
2021-10-28 21:03:21 -0300

    pylint errors have been solved

--- src/recursiveTiming/recursive_timing.py
+++ src/recursiveTiming/recursive_timing.py
@@ -1,29 +1,45 @@
 from datetime import datetime
+import numpy as np
+
 
 class RecursiveTimer():
     def __init__(self):
         self.NewTime = {}
         self.reference = {}
-    
+
     def verbose(self):
         if self.reference == {}:
             pass
         else:
             NewTimeKey = list(self.NewTime.keys())[0]
             RefereceKey = list(self.reference.keys())[0]
-            SpeedUp = (self.NewTime[NewTimeKey] / self.reference[RefereceKey])
+            SpeedUp = (self.NewTime[NewTimeKey] /
+                       self.reference[RefereceKey]
+                       )
+
             print('Reference Time: ', self.reference)
             print('New Time: ', self.NewTime)
-            print('Speed gain (new/old) {0}/{1}: {2:.2f}'.format(NewTimeKey, RefereceKey, SpeedUp))
-            print('New-Time is faster Reference-time: ', self.NewTime[NewTimeKey] < self.reference[RefereceKey])
-    
+            print(
+                'Speed gain (new/old) {0}/{1}: {2:.2f}'.format(NewTimeKey,
+                                                               RefereceKey,
+                                                               SpeedUp
+                                                               )
+            )
+            print('New-Time is faster Reference-time: ',
+                  self.NewTime[NewTimeKey] < self.reference[RefereceKey])
+
     def __call__(self, *args, **kwargs):
-            
+
         Results = self.decorator_factory(*args, **kwargs)
 
         return Results
 
-    def decorator_factory(self, argument='CPU', function=callable, *args, **kwargs):
+    def decorator_factory(
+            self,
+            argument='CPU',
+            function=callable,
+            *args,
+            **kwargs):
         self.argument = argument
         runs = kwargs.pop('runs', 5)
         print('N° Runs: ', runs)
@@ -39,12 +55,13 @@ class RecursiveTimer():
                     t1 = datetime.now()
                     dt = t1 - t0
                     dts[i] = dt.microseconds
-                    
 
-                std , average = dts.std(), dts.mean()
+                std, average = dts.std(), dts.mean()
                 self.std = std
-                self.NewTime = {argument:average}
-                print("Average time: {0:.2f}ms Std {1:.2f}ms".format(average, std))
+                self.NewTime = {argument: average}
+                print(
+                    "Average time: {0:.2f}ms Std {1:.2f}ms".format(
+                        average, std))
                 self.verbose()
                 self.reference = self.NewTime
                 return None
--- src/recursiveTiming/tests/test_recursive_timing.py
+++ src/recursiveTiming/tests/test_recursive_timing.py
@@ -3,28 +3,31 @@ import numpy as np
 from recursiveTiming.recursive_timing import RecursiveTimer
 
 
-size = 30*1e6
+size = 30 * 1e6
 
 timer = RecursiveTimer()
+
 
 def get_random_array(size=size):
     random_array = np.random.random((int(size)))
     return random_array
+
 
 random_array = get_random_array()
 
 
 output = np.zeros_like(random_array)
 
+
 def __foo(i):
     output[i] = random_array[i] * 2
     return None
+
 
 @timer('CPU', runs=3)
 def foo():
     reference = [__foo(i) for i in range(len(random_array))]
     return reference
-
 
 
 @timer('numpy_Vectorized', runs=3)
@@ -34,16 +37,15 @@ def numpy_Vectorized():
 
 
 class TestEpiWeeks(TestCase):
-        
+
     def test_RecursiveTimer(self):
         try:
-            
+
             foo()
 
             numpy_Vectorized()
-            
+
             print(timer.reference)
-            
+
         except BaseException as err:
             print(err)
-        
